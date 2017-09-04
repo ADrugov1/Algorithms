06611555cfb1b310deb8037eb0b01fa2a65bde2c
Adrug <adrug@wildapricot.com>
2017-09-04 16:47:17 +0300

    Implement randomizer

---
+++ Algorithm/Algorithm/Helpers/Randomizer.cs
@@ -0,0 +1,15 @@
+﻿namespace Algorithm.Helpers
+{
+    public class Randomizer
+    {
+        public int[] RandomArray(int arrayLength)
+        {
+            var array = new int[arrayLength];
+            var rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next();
+            }
+        }
+    }
+}
